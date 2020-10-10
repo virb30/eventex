db7486dd020c2acd390212b1fd477f177cd34e74
Vinicius Boscoa <virb30@gmail.com>
2020-10-10 13:16:44 -0300

    Update settings.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-default_dburl = 'sqlite://' / BASE_DIR / 'db.sqlite'
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl)
 }
@@ -119,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
