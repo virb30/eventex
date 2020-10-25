073e1af9b92c37f5e9b63cf49cd844b7f158e78b
Vinicius Boscoa <virb30@gmail.com>
2020-10-25 09:42:44 -0300

    add dynamic urls

--- eventex/subscriptions/tests/test_mail_subscribe.py
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -1,12 +1,13 @@
 from django.core import mail
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Vinicius Boscoa', cpf='12345678901',
                     email='valid@email.com', phone='99-99999-9999')
-        self.client.post('/inscricao/', data)
+        self.client.post(r('subscriptions:new'), data)
         self.email = mail.outbox[0]
 
     def test_subscription_email_subject(self):
